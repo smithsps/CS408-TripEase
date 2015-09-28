6ce04eca32cdf931bf03057cac0a87add1e8402a
Simon Smith <smithsps@gmail.com>
2015-09-27 20:11:39 -0400

    Refractored Base App

--- src/main/java/com/cs408/tripease/TripEaseServer.java
+++ src/main/java/com/cs408/tripease/TripEaseServer.java
@@ -1,27 +1,28 @@
-package io.vertx.example;
+package com.cs408.tripease;
 
+import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
 
-public class HelloWorldEmbedded {
+public class TripEaseServer extends AbstractVerticle{
 
   public static void main(String[] args) {
     int port;
     String hostname;
+
     try {
       port = Integer.parseInt(System.getenv("OPENSHIFT_VERTX_PORT"));
-    } catch (Exception ex) {
+    } catch (NullPointerException ex) {
       port = 8080;
     }
     
     try {
       hostname = System.getenv("OPENSHIFT_VERTX_IP");
-    } catch (Exception ex) {
+    } catch (NullPointerException nullex) {
       hostname = "localhost";
     }
 
-
     // Create an HTTP server which simply returns "Hello World!" to each request.
-    Vertx.vertx().createHttpServer().requestHandler(req -> req.response().end("Hello Brian!")).listen(port, hostname);
+    Vertx.vertx().createHttpServer()
+            .requestHandler(req -> req.response().end("CS408 : TripEase")).listen(port, hostname);
   }
-
 }
