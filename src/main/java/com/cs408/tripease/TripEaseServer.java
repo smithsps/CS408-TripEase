89cde0ec01ea1de1ab2fee208be58da4e5785ca0
Simon Smith <smithsps@gmail.com>
2015-10-04 18:49:17 -0400

    Tuned Routing

--- src/main/java/com/cs408/tripease/TripEaseServer.java
+++ src/main/java/com/cs408/tripease/TripEaseServer.java
@@ -8,6 +8,9 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.Route;
 import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.CookieHandler;
+import io.vertx.ext.web.handler.UserSessionHandler;
+import io.vertx.ext.auth.AuthProvider;
 
 
 public class TripEaseServer extends AbstractVerticle {
@@ -17,19 +20,24 @@ public class TripEaseServer extends AbstractVerticle {
     private static int port;
     private static String hostname;
 
+    private static int dbPort;
+    private static String dbHostname;
+    private static String dbUsername;
+    private static String dbPassword;
+
     @Override
     public void start() {
         Vertx vertx = Vertx.vertx();
 
         HttpServer server = Vertx.vertx().createHttpServer();
         Router router = Router.router(vertx);
-        Route route = router.route("/*");
 
-        StaticHandler staticHandler = StaticHandler.create();
-        //As we are still developing we don't want to cache files.
-        staticHandler.setCachingEnabled(false);
+        //Main Page
+        router.route().handler(StaticHandler.create());
 
-        route.handler(staticHandler);
+        //Assets routing
+        router.routeWithRegex(".+(fonts|css|images|js).*")
+              .handler(StaticHandler.create());
 
 
         System.out.println("TripEase server started at port:" + port + " on " + hostname);
@@ -49,9 +57,19 @@ public class TripEaseServer extends AbstractVerticle {
             if (hostname == null) {
                 throw new NullPointerException();
             }
-        } catch (NullPointerException | NumberFormatException nullex) {
+        } catch (NullPointerException nullex) {
             TripEaseServer.log.info("Could not find Openshift hostname, using localhost.");
             hostname = "localhost";
+        }
+
+        try {
+            dbHostname = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+            dbPort = Integer.parseInt(System.getenv("OPENSHIFT_MYSQL_DB_PORT"));
+            dbUsername = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+            dbPassword = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+        } catch (NullPointerException | NumberFormatException ex) {
+            TripEaseServer.log.fatal("Could not get database information! Exiting.. ");
+            //System.exit(0);
         }
 
         //Not sure if more preferred way to start.
