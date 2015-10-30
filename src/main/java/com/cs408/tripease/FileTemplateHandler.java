d8a2642c60372bb069874ea64cc55bdd57ccfcfe
smithsp <smithsp@purdue.edu>
2015-10-30 16:04:36 -0400

    Templating work

---
+++ src/main/java/com/cs408/tripease/FileTemplateHandler.java
@@ -0,0 +1,35 @@
+package com.cs408.tripease;
+
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.ext.web.handler.TemplateHandler;
+import io.vertx.ext.web.impl.Utils;
+import io.vertx.ext.web.templ.TemplateEngine;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.core.Handler;
+
+public class FileTemplateHandler implements Handler<RoutingContext>{
+
+    public TemplateEngine engine;
+    public String filename;
+    public String contentType;
+
+    public static FileTemplateHandler create(TemplateEngine engine, String filename) {
+        FileTemplateHandler fth = new FileTemplateHandler();
+        fth.engine = engine;
+        fth.filename = filename;
+        fth.contentType = "text/html";
+        return fth;
+    }
+
+    @Override
+    public void handle(RoutingContext context) {
+        String file = "/" + filename;
+        engine.render(context, file, res -> {
+            if (res.succeeded()) {
+                context.response().putHeader(HttpHeaders.CONTENT_TYPE, contentType).end(res.result());
+            } else {
+                context.fail(res.cause());
+            }
+        });
+    }
+}
